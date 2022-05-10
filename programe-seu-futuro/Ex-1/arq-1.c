fb2e25e711415d65ae3a6e1807e92c31e8ee2f4b
AndreLucas9310 <andr9310@gmail.com>
2022-05-09 21:18:52 -0300

    First commit!

---
+++ programe-seu-futuro/Ex-1/arq-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+//Testanto os valores que a função "printf()" retorna.
+int main(){
+	printf("valor: %d", printf("valor"));
+
+	printf("\n\n***FIM***\n\n");
+	return 0;
+};
+ 
---
+++ programe-seu-futuro/Ex-2/arq-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+//Não é nescessário usar "system("pause")", o "getchr()" Ou o "scanf()" já dão conta do recado.
+int main(){
+	printf("Presione qualquer botão para finalizar o programa.");
+	getchar();
+
+	printf("***FIM***");
+	return 0;
+};
